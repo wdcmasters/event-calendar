8be811ef82778499b3c18d47046c43c87a614b36
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-05-30 23:45:18 +0000

    Mon, May 30, 2022, 11:45 PM +00:00

--- schema_diagram.sql
+++ schema_diagram.sql
@@ -1,1 +1,8 @@
-CRE
+CREATE TABLE Event (
+   eventID ,
+   NAME VARCHAR (20)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (25) ,
+   SALARY   DECIMAL (18, 2),
+   PRIMARY KEY (ID)
+);
