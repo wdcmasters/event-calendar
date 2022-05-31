0e58510af3070ad7d640e5293f11a63c48ea90f6
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-05-31 00:00:08 +0000

    Tue, May 31, 2022, 12:00 AM +00:00

--- schema_diagram.sql
+++ schema_diagram.sql
@@ -1,13 +1,20 @@
 CREATE TABLE Event (
    eventID INT,
-   eventNAME VARCHAR (50),
-   street_no VARCHAR (10),
-   suburb VARCHAR (25),
-   state VARCHAR (25),
-   country VARCHAR (25),
+   eventNAME VARCHAR(50),
+   street_no VARCHAR(10),
+   suburb VARCHAR(25),
+   state VARCHAR(25),
+   country VARCHAR(25),
    date DATE,
    time TIME,
    userID INT,
    PRIMARY KEY (eventID),
    FOREIGN KEY (userID) REFERENCES Users(userID)
+);
+
+CREATE TABLE Users (
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(50),
+    password 
 );
