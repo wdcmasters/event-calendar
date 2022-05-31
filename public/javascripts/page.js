406ea28e6f09c64fc2e0f76066c8b99254a61ad2
Luke Stainer <72187847+Woozyfo@users.noreply.github.com>
2022-05-31 03:56:39 +0000

    Updated AJAX for signup

--- public/javascripts/page.js
+++ public/javascripts/page.js
@@ -1,5 +1,26 @@
+
+/*SIGN UP*/
 function signup()
 {
-    //Get 
+    //Store values in variables
+    let firstName = document.getElementsByName("firstName")[0].value;
+    let lastName = document.getElementsByName("lastName")[0].value;
+    let password = document.getElementsByName("password")[0].value;
+    let email = document.getElementsByName("email")[0].value;
 
+    //Make the sign up object
+    let newUser =   { first_name: firstName, last_name: lastName, password: password, email: email };
+
+    //AJAX
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        //Not necessary
+        console.log("worked");
+    };
+
+    //Open the request
+    xhttp.open("POST", "/signup");
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(newUser));
 }
