91c319c2b7ca0cd826d19d6aece3c1ac8ceb3852
Luke Stainer <72187847+Woozyfo@users.noreply.github.com>
2022-05-31 04:09:42 +0000

    Updated index.js

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,41 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', function(req, res, next) {
+  //Checking if all the fields are filled
   if ('first_name' in req.body && 'last_name' in req.body && 'email' in req.body && 'password' in req.body) {
-    if (req.body.email in users) {
+
+
+    //Checking if email is in the database
+
+
+    });
+
+    if (req.body.email in users) { //Query for this
       console.log('user exists!');
       res.sendStatus(403);
     } else {
-      users[req.body.email] = { email: req.body.email, first_name: req.body.first_name, last_name: req.body.last_name, password: req.body.password };
+      
+      //Opening connection to insert user
+      req.pool.getConnection(function(error,connection){
+        if(error){
+          console.log(error);
+          res.sendStatus(500);
+          return;
+        }
+        let query = "INSERT INTO users (first_name,last_name,email,password) VALUES (?,?,?,?);"; //Inserting user
+        connection.query(query,[req.body.first_name, req.body.last_name, req.body.email, req.body.password], function(error, rows, fields) { //Running query
+          connection.release(); // release connection
+          if (error) {
+            console.log(error);
+            res.sendStatus(500);
+            return;
+          }
+          res.end();
+        });
       res.sendStatus(200);
     }
+
+
   } else {
     console.log("Bad Request");
     res.sendStatus(400);
