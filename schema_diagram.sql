64f33d0b8543bdafb9b3bf61a81855a254cc5c04
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-05-31 00:10:38 +0000

    Tue, May 31, 2022, 12:10 AM +00:00

--- schema_diagram.sql
+++ schema_diagram.sql
@@ -1,3 +1,10 @@
+-- password won't actually be stored, it will be the hash generated from salt + password
+CREATE TABLE Users (userID INT, first_name VARCHAR(30), last_name VARCHAR(30), email VARCHAR(50), password VARCHAR(50), PRIMARY KEY (userID));
+
+CREATE TABLE Roles (userID INT, role VARCHAR(20), PRIMARY KEY (userID), FOREIGN KEY (role));
+
+CREATE TABLE Email_P
+
 CREATE TABLE Event (
    eventID INT,
    eventNAME VARCHAR(50),
@@ -11,6 +18,3 @@ CREATE TABLE Event (
    PRIMARY KEY (eventID),
    FOREIGN KEY (userID) REFERENCES Users(userID)
 );
-
--- password won't actually be stored, it will be the hash generated from salt + password
-CREATE TABLE Users (first_name VARCHAR(30), last_name VARCHAR(30), email VARCHAR(50), password VARCHAR(50));
