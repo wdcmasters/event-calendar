935256934cf43e4be0cdc8b8d71fbc2e7d04efaa
flindyly <83646358+flindyly@users.noreply.github.com>
2022-06-03 01:45:06 +0000

    Tried google auth

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,3 +1,5 @@
+
+// not recommended - want to send full encrypted token
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
