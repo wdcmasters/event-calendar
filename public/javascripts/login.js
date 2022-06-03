806be80ed75d241fb9ec7f58e06074195afc0074
flindyly <83646358+flindyly@users.noreply.github.com>
2022-06-03 13:45:11 +0000

    Implemented google auth

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,9 +1,25 @@
-
-// not recommended - want to send full encrypted token
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  // for basic testing
+  // var profile = googleUser.getBasicProfile();
+  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  // console.log('Name: ' + profile.getName());
+  // console.log('Image URL: ' + profile.getImageUrl());
+  // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  // once login is successful, send id token to server
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      alert("Login successful");
+    } else if (this.readyState == 4 && this.status >=400){
+      alert("Login failed");
+    }
+  };
+
+  xhttp.open('POST', '/users/logintest');
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.send(JSON.stringify({
+    token: googleUser.getAuthResponse().id_token
+  }));
 }
