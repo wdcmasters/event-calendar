06b61c9cacccb817f13153d08c72062c930d98a5
flindyly <83646358+flindyly@users.noreply.github.com>
2022-06-07 03:39:31 +0000

    m

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -18,9 +18,15 @@ function showPasswordChange() {
 }
 
 // sign out
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-      console.log('User signed out.');
-    });
-}
+// function signOut() {
+//     // var auth2 = gapi.auth2.getAuthInstance();
+//     // auth2.signOut().then(function () {
+//     //   console.log('User signed out.');
+//     // });
+//     const token = gapi.client.getToken();
+//     if (token !== null) {
+//         google.accounts.oauth2.revoke(token.access_token);
+//         gapi.client.setToken('');
+//         console.log('User signed out.');
+//     }
+// }
