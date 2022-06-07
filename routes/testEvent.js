470f15d920a4f65c197e57484616533bbf37c3e5
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-07 13:03:49 +0000

    Tue, Jun 7, 2022, 1:03 PM +00:00

--- routes/testEvent.js
+++ routes/testEvent.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 
 
-router.get('/event_id', function(req, res, next) {
+router.get('/event_details', function(req, res, next) {
     
+
+
 });
 
 
 router.get('/event/respond', function(req, res, next) {
 
-    res.sendFile("book_event.html", { root: path.resolve(_dirname, '/event_id') }, function(err) {
+    res.sendFile("book_event.html", { root: path.resolve(_dirname, '/event_details') }, function(err) {
         if(err) {
             //error handling
         }
