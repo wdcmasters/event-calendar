0f6d57f0041e2830903cc3b47356aa6a94a6a0f9
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-07 04:38:58 +0000

    Tue, Jun 7, 2022, 4:38 AM +00:00

--- public/javascripts/user_no_acc.js
+++ public/javascripts/user_no_acc.js
@@ -4,7 +4,20 @@ function no_acc_link() {
     var url = window.location.href;  //getting the url of the page
     var splitUrl = myString.split('/');
     var event_id = splitUrl[splitUrl.length - 1];  //event id stored into id
-    
+
+
+    //AJAX to fetch event data
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            //alert("Login successful");
+            window.location.href = '/Dashboard.html';
+        } else if (this.readyState == 4 && this.status >=400){
+            alert("Could not create account. ");
+        }
+    };
+
 }
 
 
