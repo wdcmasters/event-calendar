46b11f3591815279b0f7da4a804e1cba43649a92
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-07 04:31:05 +0000

    Tue, Jun 7, 2022, 4:31 AM +00:00

--- routes/testEvent.js
+++ routes/testEvent.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/ ', function(req, res, next) {
+router.get('/event/respond', function(req, res, next) {
 
     
+
 
 
 });
