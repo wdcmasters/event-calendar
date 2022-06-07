c026fc83f39fdfa1d99fd320f4464b9752f9f0ce
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-07 04:37:32 +0000

    Tue, Jun 7, 2022, 4:37 AM +00:00

--- public/javascripts/user_no_acc.js
+++ public/javascripts/user_no_acc.js
@@ -3,10 +3,8 @@ function no_acc_link() {
     location.href = "/event/id"
     var url = window.location.href;  //getting the url of the page
     var splitUrl = myString.split('/');
-    var answer = parts[parts.length - 1];
-
-
-
+    var event_id = splitUrl[splitUrl.length - 1];  //event id stored into id
+    
 }
 
 
