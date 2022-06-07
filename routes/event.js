7e4027f3a70e62fb16a3da33298df79373ad543b
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-06-07 03:00:47 +0000

    Tue, Jun 7, 2022, 3:00 AM +00:00

--- routes/event.js
+++ routes/event.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/login', function(req, res, next) {
+router.post('/addevent', function(req, res, next) {
 
     //Storing the login details
     let email = req.body.email;
