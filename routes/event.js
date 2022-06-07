548bb4211a05e703b49694edd8c8babe903481bd
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-06-07 23:59:32 +0000

    Tue, Jun 7, 2022, 11:59 PM +00:00

--- routes/event.js
+++ routes/event.js
@@ -41,7 +41,7 @@ router.post('/addevent', function(req, res, next) {
     // in events time table, insert start time and end time, and the user id is the user id from session
     // when inserting the rest of the details into event table, may need to use inner join
     // get last inserted time id and insert all the event details + time id into the event table
-      let query = "INSERT INTO event_times (startTime,endTime,userID,eventID) VALUES (?,?,?,?);"; //Inserting event 
+      let query = "INSERT INTO event_times (startTime,endTime,userID,eventID) VALUES (?,?,?,?);"; //Inserting event
       connection.query(query,[req.body.first_name, req.body.last_name, req.body.email, req.body.password], function(error, rows, fields)
       {
         //Running query
