2845b389f5d37dfb40fb81709249ea2cb1cede21
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-07 07:06:20 +0000

    Tue, Jun 7, 2022, 7:06 AM +00:00

--- public/javascripts/user_no_acc.js
+++ public/javascripts/user_no_acc.js
@@ -1,5 +1,5 @@
 // When user without account clicks link
-function no_acc_link() {
+function no_acc() {
     location.href = "/event/id"
     var url = window.location.href;  //getting the url of the page
     var splitUrl = myString.split('/');
@@ -11,7 +11,7 @@ function no_acc_link() {
 
     xhttp.onreadystatechange = function () {
         if(this.readyState == 4 && this.status == 200) {
-            window.location.href = '/Dashboard.html';
+            window.location.href = '';
         } else if (this.readyState == 4 && this.status >=400){
 
         }
