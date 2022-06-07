62f62a71294d4d41fbc5e8b985f5e61cac273738
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-07 17:08:07 +0000

    Tue, Jun 7, 2022, 5:08 PM +00:00

--- public/javascripts/user_no_acc.js
+++ public/javascripts/user_no_acc.js
@@ -51,6 +51,27 @@ function guest() {
 }
 
 
+function guest() {
+    let firstName = document.getElementsByName("firstName")[0].value;
+    let lastName = document.getElementsByName("lastName")[0].value;
+    let guest =   { first_name: firstName, last_name: lastName };
+
+    //AJAX
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            window.location.href = "book_event.html";
+        } else if (this.readyState == 4 && this.status >=400){
+            alert("Could not create guest");
+        }
+    };
+
+    //Open the request
+    xhttp.open("GET", "/guestDetails");
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+}
 
 
 
