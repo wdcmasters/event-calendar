962a35cb8796c458540f468c5725cd30461dcdac
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-06-07 04:05:21 +0000

    Tue, Jun 7, 2022, 4:05 AM +00:00

--- routes/event.js
+++ routes/event.js
@@ -16,56 +16,57 @@ router.post('/addevent', function(req, res, next) {
     let fin_time = req.body.fin_time;
 
     console.log(req.body.eventName);
+    res.end();
     //Parsing  (do later)
     //
     //
 
     //Making sure fields are filled
-    if (email == "" || password == "")
-    {
-      console.log("Fill in the inputs");
-      res.sendStatus(404);
-      return;
-    }
+    // if (email == "" || password == "")
+    // {
+    //   console.log("Fill in the inputs");
+    //   res.sendStatus(404);
+    //   return;
+    // }
 
     //Opening connection to check logins
-    req.pool.getConnection(function(error,connection) {
-      if(error)
-      {
-        console.log(error);
-        res.sendStatus(500);
-        return;
-      }
+    // req.pool.getConnection(function(error,connection) {
+    //   if(error)
+    //   {
+    //     console.log(error);
+    //     res.sendStatus(500);
+    //     return;
+    //   }
 
-      let query = "SELECT email,password FROM users WHERE email = ? AND password = ?"; //Inserting user
-      connection.query(query,[email, password], function(error, rows, fields)
-      {
-        //Running query
-        connection.release(); // release connection
-        if (error) {
-          console.log(error);
-          console.log("Could not alert");
-          res.sendStatus(500);
-          return;
-        }
+    //   let query = "SELECT email,password FROM users WHERE email = ? AND password = ?"; //Inserting user
+    //   connection.query(query,[email, password], function(error, rows, fields)
+    //   {
+    //     //Running query
+    //     connection.release(); // release connection
+    //     if (error) {
+    //       console.log(error);
+    //       console.log("Could not alert");
+    //       res.sendStatus(500);
+    //       return;
+    //     }
 
-        if (rows.length > 0) {
-          console.log("login success");
+    //     if (rows.length > 0) {
+    //       console.log("login success");
 
-          //Associating user with session
-          req.session.authenticated = true;
-          req.session.user = { email: email, password: password };
+    //       //Associating user with session
+    //       req.session.authenticated = true;
+    //       req.session.user = { email: email, password: password };
 
-          console.log(req.session.user);
+    //       console.log(req.session.user);
 
-          //Redirecting to dashboard
-          res.redirect('/Dashboard.html');
-          return;
-        }
+    //       //Redirecting to dashboard
+    //       res.redirect('/Dashboard.html');
+    //       return;
+    //     }
 
-        res.sendStatus(401);
-      });
-    });
+    //     res.sendStatus(401);
+    //   });
+    // });
 
   });
 
