c8cd0f3b1577c24f59eaf6324675c27a5c63e19b
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-07 04:35:35 +0000

    Tue, Jun 7, 2022, 4:35 AM +00:00

--- public/javascripts/user_no_acc.js
+++ public/javascripts/user_no_acc.js
@@ -1,6 +1,9 @@
 // When user without account clicks link
 function no_acc_link() {
-    
+    location.href = "/event/id"
+    var url = window.location.href;  //getting the url of the page
+    var splitUrl = myString.split('/');
+    var answer = parts[parts.length - 1];
 
 
 
