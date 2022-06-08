df2a8cbb4433cd4435762b11d78f6dd0fff0b284
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-06-08 00:32:24 +0000

    Wed, Jun 8, 2022, 12:32 AM +00:00

--- routes/event.js
+++ routes/event.js
@@ -41,6 +41,8 @@ router.post('/addevent', function(req, res, next) {
     // in events time table, insert start time and end time, and the user id is the user id from session
     // when inserting the rest of the details into event table, may need to use inner join
     // get last inserted time id and insert all the event details + time id into the event table
+
+    // how to generate eventID?
     let event_query = "INSERT INTO event (eventID, eventName, street_no, street, suburb, state, post_code, country, date, userID) VALUES (?,?,?,?,?,?,?,?,?,?);";
     connection.query(query,[req.body.eventName, req.body.street_no, req.body.street, req.body.city, req.body.state, req.body.post_code, req.body.country, req.body.date, req.body.start_time , req.body.fin_time], function(error, rows, fields)
 
