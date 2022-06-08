1792e7b23f875714b9e2b7fcc2d683bb6580153d
samanthakuan <69128127+samanthakuan@users.noreply.github.com>
2022-06-08 01:03:57 +0000

    Wed, Jun 8, 2022, 1:03 AM +00:00

--- routes/event.js
+++ routes/event.js
@@ -46,7 +46,7 @@ router.post('/addevent', function(req, res, next) {
     let event_query = "INSERT INTO event (eventID, eventName, street_no, street, suburb, state, post_code, country, date, userID) VALUES (?,?,?,?,?,?,?,?,?,?);";
     connection.query(query,[req.body.eventName, req.body.street_no, req.body.street, req.body.city, req.body.state, req.body.post_code, req.body.country, req.body.date, req.body.start_time , req.body.fin_time], function(error, rows, fields)
 
-      let query = "SELECT userID FROM users_events INNER JOIN users WHERE users.userID = users_events.userID INSERT INTO event_times (startTime,endTime,userID,eventID) VALUES (?,?,?,?);"; //Inserting event
+      let query = "SELECT userID FROM users_events INNER JOIN users WHERE users.userID = users_events.userID INSERT INTO event_times (start_time,end_time,eventID) VALUES (?,?,?);"; //Inserting event
       connection.query(query,[req.body.first_name, req.body.last_name, req.body.email, req.body.password], function(error, rows, fields)
       {
         //Running query
