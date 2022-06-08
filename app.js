b005b19319bf7c2af68663f5e654b69789bdbcc7
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-08 05:11:34 +0000

    Wed, Jun 8, 2022, 5:11 AM +00:00

--- app.js
+++ app.js
@@ -109,14 +109,6 @@ app.use('/event', eventRouter);
 // });
 
 
-// app.get('/event/respond', (req, res) => {
-
-//   res.sendFile("book_event.html", { root: path.resolve(_dirname, '/event/:id') }, function(err) {
-//       if(err) {
-//         res.sendStatus(500); //error handling
-//       }
-//   });
-// });
 
 
 /*app.post('/event/respond', (req, res) => {
--- public/javascripts/user_no_acc.js
+++ public/javascripts/user_no_acc.js
@@ -1,32 +1,4 @@
 // user without account clicks link
-/*
-function no_acc() {
-
-    location.href = "<url>/event/id". // setting url
-    var url = window.location.href;  //getting the url of the page
-    var splitUrl = myString.split('/');
-    var eventId = splitUrl[splitUrl.length - 1];  //event id stored into id
-
-
-    //AJAX to fetch event data
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function () {
-        if(this.readyState == 4 && this.status == 200) {
-
-
-        } else if (this.readyState == 4 && this.status >=400){
-
-        }
-    };
-
-    xhttp.open("GET", "/event_details");
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(JSON.stringify(eventId));
-
-
-}
-*/
 
 function guest() {
     let firstName = document.getElementsByName("firstName")[0].value;
@@ -61,7 +33,7 @@ function guest() {
 
     xhttp.onreadystatechange = function () {
         if(this.readyState == 4 && this.status == 200) {
-            window.location.href = "pop_up_guest.html";
+            window.location.href = ".html";
         } else if (this.readyState == 4 && this.status >=400){
             alert("Could not create guest");
         }
@@ -72,7 +44,6 @@ function guest() {
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 }
-
 
 
 
