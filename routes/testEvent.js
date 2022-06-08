faa047fdfcdfaf8fd3ba562c008ffa39c4561fc9
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-08 03:07:06 +0000

    Wed, Jun 8, 2022, 3:07 AM +00:00

--- routes/testEvent.js
+++ routes/testEvent.js
@@ -2,7 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 
+router.post('/event/respond', (req, res) => {
+    if (req.body.first_name == "" || req.body.last_name == "")
+    {
+      console.log("Fill in the inputs");
+      res.sendStatus(404);
+      return;
+    }
+    if ('first_name' in req.body && 'last_name' in req.body) {
+      //Opening connection
+      req.pool.getConnection(function(error,connection) {
+        if(error) {
+          console.log(error);
+          res.sendStatus(500);
+          return;
+        }
 
+        let query = "INSERT INTO users (first_name,last_name) VALUES (?,?);"; //Inserting guest into db
+        connection.query(query,[req.body.first_name, req.body.last_name], function(error, rows, fields)
+        {
+          //Running query
+          connection.release(); // release connection
+          if (error) {
+            console.log(error);
+            res.sendStatus(500);
+            return;
+
+            res.redirect("book_event.html");
+          }
+
+        });
+      });
+    }
+  });
 
 
 
