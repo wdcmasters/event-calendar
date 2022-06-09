58516e335b7b6a40f71800edb3db6d9871ec3a33
ajeendrap <83900667+ajeendrap@users.noreply.github.com>
2022-06-09 15:10:48 +0000

    Thu, Jun 9, 2022, 3:10 PM +00:00

--- public/javascripts/user_no_acc.js
+++ public/javascripts/user_no_acc.js
@@ -18,7 +18,7 @@ function guest() {
     };
 
     //Open the request
-    xhttp.open("POST", "/event/respond/guest?eventID="+req.session.eventID);
+    xhttp.open("POST", "/event/respond/guest?eventID=1");
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(JSON.stringify(guest));
 }
